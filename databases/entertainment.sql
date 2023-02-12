49862ce4b076dffedc944e900bd3f58e38aef1fb
almaduri <ali.mdrw@gmail.com>
2023-02-12 20:52:33 +0700

    add Skeleton Knight in Another World

--- databases/entertainment.sql
+++ databases/entertainment.sql
@@ -26,7 +26,7 @@ CREATE TABLE `watched_anime` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=177 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=178 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -211,7 +211,8 @@ INSERT INTO `watched_anime` VALUES
 (173,'Chainsaw Man'),
 (174,'The Rising of the Shield Hero'),
 (175,'The Rising of the Shield Hero Season 2'),
-(176,'Record of Ragnarok Season 2');
+(176,'Record of Ragnarok Season 2'),
+(177,'Skeleton Knight in Another World');
 
 /*!40000 ALTER TABLE `watched_anime` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -225,4 +226,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-12  0:18:32
+-- Dump completed on 2023-02-12 20:52:21
